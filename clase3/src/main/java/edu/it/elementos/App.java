b86072b84bb6299d39cf8eb2deab0347ec5a045a
andy <arteysoft@gmail.com>
2023-07-04 22:05:05 -0300

    clase #3

--- clase3/src/main/java/edu/it/elementos/App.java
+++ clase3/src/main/java/edu/it/elementos/App.java
@@ -1,8 +1,8 @@
 package edu.it.elementos;
 
-import com.inversiondecontrol.ejemplo.HolaMundoImpl;
-import com.observer.problema.Observable;
-import com.observer.problema.Observador;
+import com.modeloactor.ejemplo.Actor;
+import com.modeloactor.ejemplo.MiPrimerActor;
+import com.modeloactor.ejemplo.SegundoActor;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -11,8 +11,13 @@ public class App {
         // var hm = new HolaMundoImpl();
         // hm.aceptarConexion();
         
-        var obs = new Observable();
-        obs.addNotificable(new Observador());
-        obs.run();
+        // var obs = new Observable();
+        // obs.addNotificable(new Observador());
+        // obs.run();
+        
+        Actor segundoActor = new SegundoActor();
+        Actor miPrimerActor = new MiPrimerActor(segundoActor);
+        
+        miPrimerActor.encolarMensaje("100");
     }
 }
